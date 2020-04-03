0c42e18bc3a0ef12508ae26570586f7b3e49c3a3
Tedhoon <gt0305@likelion.org>
2020-04-04 06:44:19 +0900

    [add] 시 군구 연결

--- assembly/views.py
+++ assembly/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from get_data.models import Party, PartyPolicy, City, Gungu, Candidate 
 import urllib.request
-from urllib.parse import  urlencode,quote_plus
+from urllib.parse import urlencode,quote_plus
 
 
 # Create your views here.
 def index(request):
     cities = City.objects
-    gungus = Gungu.objects
+    gungus = ""
     candidates = Candidate.objects
     parties = Party.objects
     partypolicies = PartyPolicy.objects
+    try:
+        if request.GET['cities']:
+            gungus=Gungu.objects.filter(sd_name_id=request.GET['cities'])
+    except:
+        print("pass")
+        pass
     return render(request, 'index.html', 
     {'cities' : cities, 'gungus' :gungus, 'candidates':candidates, 'parties':parties, 'partypolicies':partypolicies})
