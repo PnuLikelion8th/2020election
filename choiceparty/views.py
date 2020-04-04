454b089347f3a2cb6bccade64cf590a452c41016
chichi <jang.chihun@gmail.com>
2020-04-04 16:26:07 +0900

    [추가] top-bottom 구분 및 부전승, random부분 추가

--- choiceparty/views.py
+++ choiceparty/views.py
@@ -6,9 +6,32 @@ def blind_party(request):
     # temp_list=list(range(1, 52))
     # random.shuffle(temp_list)
     # print(temp_list)
-    all_party = Party.objects.prefetch_related('partypolicy_set').all().order_by('?')
+    all_party = Party.objects.exclude(name="무소속").prefetch_related('partypolicy_set').order_by('?')
 
+    len_party = len(all_party)
+
+    if len_party % 2 == 1:
+        msg = "???정당이 부전승으로 올라갔습니다."
+        unearned = all_party[len_party-1]
+
+    else:
+        msg = None
+        unearned = None
     
-    context = {'all_party': all_party}
+    divider_num = len_party//2
+    
+    top = all_party[0:divider_num]
+    bottom = all_party[divider_num:len_party-1]
+
+    for i,k in enumerate(top):
+        print(i,k)
+
+    print("="*100)
+
+    for j,v in enumerate(bottom):
+        print(j,v)
+
+    context = {'all_party': all_party, 'len_party': len_party,
+               'top': top, 'bottom': bottom, 'msg': msg, 'unearned': unearned}
 
     return render(request , 'blind_party.html',context)
