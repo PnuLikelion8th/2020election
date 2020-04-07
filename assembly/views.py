28f9cf5864c546b2d682309638977d4a1836be5a
hyerimsn <etcheef@likelion.org>
2020-04-07 17:52:25 +0900

    후보자 필터링 중

--- assembly/views.py
+++ assembly/views.py
@@ -8,15 +8,18 @@ from urllib.parse import urlencode,quote_plus
 def index(request):
     cities = City.objects
     gungus = None
-    candidates = Candidate.objects
+    candidates = None
     parties = Party.objects
     partypolicies = PartyPolicy.objects
     
 
     if request.GET.get('gungus'):
         gungus=Gungu.objects.filter(sd_name = City.objects.get(name=request.GET['bf_cities']))
+        # .get(name=request.GET['gungus']))
         temp_cities = request.GET.get('bf_cities')
         temp_gungus = request.GET.get('gungus')
+        # candidates = Candidate.objects.filter(sggname = Gungu.objects.get(name=request.GET['gungus']).sgg_name)
+        candidates = Candidate.objects.filter(sggname = Gungu.objects.get(name=request.GET['gungus']).sgg_name)
         context = {'cities' : cities, 'temp_cities':temp_cities, 'gungus' :gungus,'temp_gungus':temp_gungus, 'candidates':candidates, 'parties':parties, 'partypolicies':partypolicies}
         return render(request, 'index.html', context)
         pass
@@ -31,4 +34,10 @@ def index(request):
     temp_cities = None
     temp_gungus = None
     context = {'cities' : cities, 'temp_cities':temp_cities, 'gungus' :gungus, 'candidates':candidates, 'parties':parties, 'partypolicies':partypolicies}
+    return render(request, 'index.html', context)
+
+
+def who(request):
+    whos = Candidate.objects.filter(sggname= Candidate.objects.get(name=request.GET['temp_gungus']))
+    context = {'whos':whos}
     return render(request, 'index.html', context)
