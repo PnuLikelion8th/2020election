6d370e612463cb22591cabcf567e6a2492eca3b9
chichi <jang.chihun@gmail.com>
2020-04-08 12:17:14 +0900

    [추가] 결과화면 가운데 정렬 및 배포 경로 문제

--- election/wsgi.py
+++ election/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'election.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'election.settings.deploy')
 
 application = get_wsgi_application()
