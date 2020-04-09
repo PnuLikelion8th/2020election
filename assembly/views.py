27a66a0921f510cc5a8ab95108e3158d7cf862e2
Tedhoon <gt0305@likelion.org>
2020-04-10 00:22:32 +0900

    [mdf] 리팩토링

--- assembly/views.py
+++ assembly/views.py
@@ -11,21 +11,12 @@ def index(request):
     candidates = Candidate.objects
     parties = Party.objects
     partypolicies = PartyPolicy.objects
-    print(gungus)
 
     if request.GET.get('gungus'):
         temp_gungus = Gungu.objects.get(id=request.GET.get('gungus'))
-        print(temp_gungus)
         target_candidates = candidates.filter(sggname=temp_gungus)
-        # except:
-        #     target_candidates = None
-        # print([qs if qs.Exist else 'None' for qs in candidates.filter(sggname=Gungu.objects.get(name=temp_gungus))])
         context = {'cities' : cities, 'gungus' :gungus,'temp_gungus':temp_gungus, 'candidates':candidates,'target_candidates':target_candidates, 'parties':parties, 'partypolicies':partypolicies}
         return render(request, 'index.html', context)
 
-
-    
-
-    # temp_cities = request.GET.get('cities')
     context = {'cities' : cities, 'gungus' :gungus, 'candidates':candidates, 'parties':parties, 'partypolicies':partypolicies}
     return render(request, 'index.html', context)
